db4cb03ce22e8e9d39e720a2d69d4064de55fa2d
Samir Silva <samirsilva@MacBook-Air-de-Samir.local>
2020-05-29 14:49:24 -0300

    Updates

---
+++ precache-manifest.081a5fe47c7d97d6bcc05254671e7c4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05866a1c860154e7e9999308d34fc7c9",
+    "url": "/organizacional/index.html"
+  },
+  {
+    "revision": "babcadf2d781f30560c4",
+    "url": "/organizacional/static/css/main.f9e9cbaa.chunk.css"
+  },
+  {
+    "revision": "b5fff60aa02dac7c25f5",
+    "url": "/organizacional/static/js/2.93086120.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/organizacional/static/js/2.93086120.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "babcadf2d781f30560c4",
+    "url": "/organizacional/static/js/main.9d64ba15.chunk.js"
+  },
+  {
+    "revision": "f18a84905f3bcbff8d11",
+    "url": "/organizacional/static/js/runtime-main.12d2c6e7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/organizacional/static/media/logo.5d5d9eef.svg"
+  }
+]);
